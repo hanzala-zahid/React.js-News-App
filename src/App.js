3ef095d748d01fe7577997bfa13e7c0a54599362
Hanzala Zahid <hanzalazahid93@gmail.com>
2022-05-31 06:23:26 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  const pageSize = 6;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route
+            exact
+            path="/home"
+            element={
+              <News
+                key="general"
+                pageSize={pageSize}
+                country="us"
+                category="general"
+              />
+            }
+          />
+
+          <Route
+            exact
+            path="/business"
+            element={
+              <News
+                key="business"
+                pageSize={pageSize}
+                country="us"
+                category="business"
+              />
+            }
+          />
+
+          <Route
+            exact
+            path="/entertainment"
+            element={
+              <News
+                key="entertainment"
+                pageSize={pageSize}
+                country="us"
+                category="entertainment"
+              />
+            }
+          />
+
+          <Route
+            exact
+            path="/sports"
+            element={
+              <News
+                key="sports"
+                pageSize={pageSize}
+                country="us"
+                category="sports"
+              />
+            }
+          />
+
+          <Route
+            exact
+            path="/science"
+            element={
+              <News
+                key="science"
+                pageSize={pageSize}
+                country="us"
+                category="science"
+              />
+            }
+          />
+
+          <Route
+            exact
+            path="/technology"
+            element={
+              <News
+                key="technology"
+                pageSize={pageSize}
+                country="us"
+                category="technology"
+              />
+            }
+          />
+
+          <Route
+            exact
+            path="/health"
+            element={
+              <News
+                key="health"
+                pageSize={pageSize}
+                country="us"
+                category="health"
+              />
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
-}
-
+};
 export default App;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <span
+          className="position-absolute top-0 translate-middle badge rounded-pill bg-dark"
+          style={{ left: "90%", zIndex: "1" }}
+        >
+          {source}
+        </span>
+        <img
+          src={
+            !imageUrl
+              ? "https://local12.com/resources/media/55b7dca2-eca2-44dd-bc06-962973c5c702-large16x9_AP22096613260252.jpg?1649327001260"
+              : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {!author ? "Unknown" : author} on{" "}
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            href={newsUrl}
+            rel="noreferrer"
+            target="_blank"
+            className="btn btn-sm btn-dark"
+          >
+            Read More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
